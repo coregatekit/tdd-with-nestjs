84a5ac41ada7a53b5cd243704bdbe3573aabfdf8
Joe Kim <joesm.genie@gmail.com>
2024-05-29 15:12:39 +0700

    Inject prisma service for user service

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly _prismaService: PrismaService;
+
+  /**
+   * Constructor of the UsersService that inject prisma service
+   * @param prismaService The prisma service
+   */
+  constructor(private prismaService: PrismaService) {
+    this._prismaService = this.prismaService;
+  }
+}
