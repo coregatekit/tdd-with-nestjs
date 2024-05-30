d6941705e3c60270e4350e0ce307204ec7b0eb65
Joe Kim <joesm.genie@gmail.com>
2024-05-30 13:32:03 +0700

    Add method find ser by email in controller

--- src/modules/users/users.controller.spec.ts
+++ src/modules/users/users.controller.spec.ts
@@ -1,18 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { IUserResponse } from './users.interface';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
+  const mockUsersService = {
+    findUserByEmail: jest.fn(),
+    register: jest.fn(),
+  };
+
+  const RealDate = Date.now;
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+
+    global.Date.now = jest.fn(() => new Date('2024-01-23T10:20:30Z').getTime());
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findUserByEmail', () => {
+    it('should return a user', async () => {
+      const expectedResult: IUserResponse = {
+        id: '1',
+        name: 'John Doe',
+        email: 'john@example.com',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+      mockUsersService.findUserByEmail.mockResolvedValue(expectedResult);
+
+      const result = await controller.findUserByEmail('john@example.com');
+
+      expect(result).toEqual(expectedResult);
+    });
+  });
+
+  afterEach(() => {
+    global.Date.now = RealDate;
   });
 });
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  private readonly _usersService: UsersService;
+
+  /**
+   * A constructor of the UsersController that injects the UsersService
+   */
+  constructor(private userService: UsersService) {
+    this._usersService = this.userService;
+  }
+
+  @Get(':email')
+  async findUserByEmail(@Param('email') email: string) {
+    return this._usersService.findUserByEmail(email);
+  }
+}
