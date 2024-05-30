2af994c551f7769fc6ce2e66df3e06a33b3b18a4
Joe Kim <joesm.genie@gmail.com>
2024-05-30 16:10:57 +0700

    Add comment in user controller

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -13,16 +13,32 @@ export class UsersController {
     this._usersService = this.userService;
   }
 
+  /**
+   * Controller Method GET that find user by email
+   * @param email The email @type string
+   * @returns The user response @interface IUserResponse
+   */
   @Get(':email')
   async findUserByEmail(@Param('email') email: string) {
     return this._usersService.findUserByEmail(email);
   }
 
+  /**
+   * Controller Method POST that register user
+   * @param registerUserDTO @interface RegisterUserDTO
+   * @returns The user response @interface IUserResponse
+   */
   @Post()
   async register(@Body() registerUserDTO: RegisterUserDTO) {
     return this._usersService.register(registerUserDTO);
   }
 
+  /**
+   * Controller Method PATCH that update user
+   * @param id is user id @type string
+   * @param updateUserDTO is request body to update user information
+   * @returns The user response @interface IUserResponse
+   */
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateUserDTO: UpdateUserDTO) {
     return this._usersService.update(id, updateUserDTO);
