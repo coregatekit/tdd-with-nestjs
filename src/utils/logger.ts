182190a40b3c55958681d04b01b444a3e6d851e4
Joe Kim <joesm.genie@gmail.com>
2024-05-31 14:12:16 +0700

    Update logger config

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,30 +5,27 @@ import {
 } from 'nest-winston';
 
 export const LoggerFactory = (appName: string) => {
-  let consoleFormat;
-
-  const DEBUG = process.env.DEBUG || true;
-  const USE_JSON_LOGGER = process.env.USE_JSON_LOGGER || 'true';
-
-  if (USE_JSON_LOGGER === 'true') {
-    consoleFormat = format.combine(
-      format.ms(),
-      format.timestamp(),
-      format.json(),
-    );
-  } else {
-    consoleFormat = format.combine(
-      format.timestamp(),
-      format.ms(),
-      nestWinstonModuleUtilities.format.nestLike(appName, {
-        colors: true,
-        prettyPrint: true,
-      }),
-    );
-  }
+  const DEBUG = process.env.DEBUG || 'true';
 
   return WinstonModule.createLogger({
-    level: DEBUG ? 'debug' : 'info',
-    transports: [new transports.Console({ format: consoleFormat })],
+    level: DEBUG === 'true' ? 'debug' : 'info',
+    transports: [
+      new transports.Console({
+        format: format.combine(
+          format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+          format.colorize({
+            message: true,
+            colors: {
+              info: 'green',
+              error: 'red',
+              warn: 'yellow',
+            },
+          }),
+          nestWinstonModuleUtilities.format.nestLike(appName, {
+            prettyPrint: true,
+          }),
+        ),
+      }),
+    ],
   });
 };
