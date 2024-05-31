eaa32d0f3aef8f5f2dc11d2f480ecc4e35dc9277
Joe Kim <joesm.genie@gmail.com>
2024-05-31 11:28:37 +0700

    Initial logger module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { LoggerModule } from './modules/logger/logger.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule],
+  imports: [UsersModule, PrismaModule, LoggerModule],
   controllers: [AppController],
   providers: [AppService],
 })
